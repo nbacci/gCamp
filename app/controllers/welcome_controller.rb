8a5682835c980b8b88765db5be95d65a6488374e
nbacci <nmbachvarova@ucdavis.edu>
2014-11-24 15:00:22 -0800

    users can see quotes on the homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def home
+    @quotes= [
+      '"Failure is not an option. Everyone has to Succeed." - Arnold Schwarzenegger',
+      '"Your time is limited, so don\'t waste it living someone\'s life." - Steve Jobs',
+      '"Better Ingredients, Better Pizza." - Papa John\'s',
+      ]
   end
 
   def about
