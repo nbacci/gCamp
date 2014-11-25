14a97da3ac4edf3f6a1db935d1f0275d86c81b05
nbacci <nmbachvarova@ucdavis.edu>
2014-11-24 16:22:51 -0800

    users can see author names seperately from quotes on the home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
   def home
     @quotes= [
-      '"Failure is not an option. Everyone has to Succeed." - Arnold Schwarzenegger',
-      '"Your time is limited, so don\'t waste it living someone\'s life." - Steve Jobs',
-      '"Better Ingredients, Better Pizza." - Papa John\'s',
+      ['"Failure is not an option. Everyone has to Succeed."', '- Arnold Schwarzenegger'],
+      ['"Your time is limited, so don\'t waste it living someone\'s life."', '- Steve Jobs'],
+      ['"Better Ingredients, Better Pizza."', '- Papa John\'s'],
       ]
   end
 
