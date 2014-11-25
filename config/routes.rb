902e5bfd7fa611e69fe98ef9827146d8727787d1
nbacci <nmbachvarova@ucdavis.edu>
2014-11-24 17:06:57 -0800

    creates the FAQ page and links it in the footer

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'terms' => 'welcome#terms'
 
+  get 'faq' => 'welcome#faq'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
